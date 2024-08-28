8e1fe636d4419dd09b062b2801ef60af9f73ab89
Stefan Jocic <sjocic@bettercollective.com>
2024-08-28 13:57:58 +0200

    spec edited

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -1,14 +1,21 @@
-beforeEach(() => {
-  cy.viewport(1920, 1080);
-  cy.visit('https://prediktor-staging.bettingexpert.com/');
-  cy.wait(500);
-  Cypress.on('uncaught:exception', (err, runnable) => {
-    return false;
-  });
-});
+// beforeEach(() => {
+//   cy.viewport(1920, 1080);
+//   cy.visit('https://prediktor-staging.bettingexpert.com/');
+//   cy.wait(500);
+//   Cypress.on('uncaught:exception', (err, runnable) => {
+//     return false;
+//   });
+// });
 
 describe('CI CD', () => {
   it('login', () => {
+    cy.viewport(1920, 1080);
+    cy.visit('https://prediktor-staging.bettingexpert.com/');
+    cy.wait(500);
+    Cypress.on('uncaught:exception', (err, runnable) => {
+      return false;
+    });
+
     cy.get('div.justify-center.gap-2 button:nth-child(1)').should('be.visible').click().then(() => {
       cy.get('div.m-4 img[alt="logo"]').should('be.visible');
     });
