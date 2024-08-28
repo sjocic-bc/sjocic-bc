78289433d3f229da708a31745caa5927465c9a37
Stefan Jocic <sjocic@bettercollective.com>
2024-08-28 15:09:40 +0200

    spec

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,12 @@
-module.exports = {
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  watchForFileChanges: false,
+  defaultCommandTimeout: 10000,
+
   e2e: {
-    specPattern: 'cypress/e2e/**/*.cy.js', 
-    baseUrl: 'http://localhost:8080', 
+    specPattern: 'cypress/e2e/**/*.cy.js',
+    baseUrl: 'http://localhost:8080',
+    pageLoadTimeout: 120000,
   },
-}
+});
--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -10,18 +10,19 @@
 describe('CI CD', () => {
   it('login', () => {
     cy.viewport(1920, 1080);
-    cy.visit('https://prediktor-staging.bettingexpert.com/');
-    cy.wait(500);
-    Cypress.on('uncaught:exception', (err, runnable) => {
-      return false;
-    });
+    cy.visit('/');
+    cy.get('h1').should('have.text', 'todos');
+    // cy.wait(500);
+    // Cypress.on('uncaught:exception', (err, runnable) => {
+    //   return false;
+    // });
 
-    cy.get('div.justify-center.gap-2 button:nth-child(1)').should('be.visible').click().then(() => {
-      cy.get('div.m-4 img[alt="logo"]').should('be.visible');
-    });
-    cy.get('input[name="email"]').should('be.visible').type('sjocic@bettercollective.com');
-    cy.get('input[name="password"]').should('be.visible').type('Better2022');
-    cy.get('button[type="submit"]').should('not.be.disabled').click();
+    // cy.get('div.justify-center.gap-2 button:nth-child(1)').should('be.visible').click().then(() => {
+    //   cy.get('div.m-4 img[alt="logo"]').should('be.visible');
+    // });
+    // cy.get('input[name="email"]').should('be.visible').type('sjocic@bettercollective.com');
+    // cy.get('input[name="password"]').should('be.visible').type('Better2022');
+    // cy.get('button[type="submit"]').should('not.be.disabled').click();
 
   });
 });
