9617aa962cc75734ffba31f898490c03621a83a3
Stefan Jocic <sjocic@bettercollective.com>
2024-08-28 12:13:29 +0200

    first commit

---
+++ cypress.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  projectId: '4qto2e',
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config)
+    },
+    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
+  },
+}
---
+++ cypress/e2e/app.spec.js
@@ -0,0 +1,20 @@
+beforeEach(() => {
+  cy.viewport(1920, 1080);
+  cy.visit('https://prediktor-staging.bettingexpert.com/');
+  cy.wait(500);
+  Cypress.on('uncaught:exception', (err, runnable) => {
+    return false;
+  });
+});
+
+describe('CI CD', () => {
+  it('login', () => {
+    cy.get('div.justify-center.gap-2 button:nth-child(1)').should('be.visible').click().then(() => {
+      cy.get('div.m-4 img[alt="logo"]').should('be.visible');
+    });
+    cy.get('input[name="email"]').should('be.visible').type('sjocic@bettercollective.com');
+    cy.get('input[name="password"]').should('be.visible').type('Better2022');
+    cy.get('button[type="submit"]').should('not.be.disabled').click();
+
+  });
+});
