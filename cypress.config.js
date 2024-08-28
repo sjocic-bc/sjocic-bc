436827cf2e360105edd9447a617508fdd7890178
Stefan Jocic <sjocic@bettercollective.com>
2024-08-28 13:51:05 +0200

    con

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   e2e: {
-    specPattern: 'cypress/e2e/**/*.spec.js',
-    baseUrl: 'http://localhost:8080',
-    // Add other configurations here
+    specPattern: 'cypress/e2e/**/*.cy.js', 
+    baseUrl: 'http://localhost:8080', 
   },
 }
