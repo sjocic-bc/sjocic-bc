489abf673a5cd3225e014a2f78df8fc2f177e4e1
Stefan Jocic <sjocic@bettercollective.com>
2025-02-26 12:56:41 +0100

    fixed

--- cypress.config.js
+++ cypress.config.js
@@ -6,7 +6,7 @@ module.exports = defineConfig({
   reporter: "mochawesome",
   reporterOptions: {
     reportDir: "cypress/results",
-    overwrite: true,
+    overwrite: false,
     html: false,
     json: true,
   },
